01d881fdab3022ec4a13022d23e0955e087b244b
AndriySV <A.S.Elips@gmail.com>
2014-09-02 10:42:06 +0300

    Create UserService.java  UserServiceImpl.java

--- src/main/java/com/softserveinc/softtour/service/Impl/UserServiceImpl.java
+++ src/main/java/com/softserveinc/softtour/service/Impl/UserServiceImpl.java
@@ -1,4 +1,46 @@
-package com.sovtserveinc.softtour.service.Impl;
+package com.softserveinc.softtour.service.Impl;
 
-public class UserServiceImpl {
+import java.sql.Date;
+import java.util.List;
+
+import com.softserveinc.softtour.dao.UserDao;
+import com.softserveinc.softtour.dto.Sex;
+import com.softserveinc.softtour.entity.User;
+import com.softserveinc.softtour.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+	private UserDao userDao;
+	
+	public void setUserDao(UserDao userDao) {
+		this.userDao = userDao;
+	}
+
+	@Override
+	public void save(String name, String e_mail, String password,
+			Date birthday, byte age, Sex sex, String phone, long role_id) {
+		userDao.save(name, e_mail, password, birthday, age, sex, phone, role_id);
+	}
+
+	@Override
+	public void update(long id, String name, String e_mail, String password,
+			Date birthday, byte age, Sex sex, String phone, long role_id) {
+		userDao.update(id, name, e_mail, password, birthday, age, sex, phone, role_id);
+	}
+
+	@Override
+	public void delete(long id) {
+		userDao.delete(id);
+	}
+
+	@Override
+	public User findById(long id) {
+		return userDao.findById(id);
+	}
+
+	@Override
+	public List<User> getAll() {
+		return userDao.getAll();
+	}
+
 }
--- src/main/java/com/softserveinc/softtour/service/UserService.java
+++ src/main/java/com/softserveinc/softtour/service/UserService.java
@@ -1,4 +1,16 @@
-package com.sovtserveinc.softtour.service;
+package com.softserveinc.softtour.service;
+
+import java.sql.Date;
+import java.util.List;
+
+import com.softserveinc.softtour.dto.Sex;
+import com.softserveinc.softtour.entity.User;
 
 public interface UserService {
+	
+	void save(String name, String e_mail, String password, Date birthday, byte age, Sex sex, String phone, long role_id);
+	void update(long id, String name, String e_mail, String password, Date birthday, byte age, Sex sex, String phone, long role_id);
+	void delete(long id);
+	User findById(long id);
+	List<User> getAll();
 }
