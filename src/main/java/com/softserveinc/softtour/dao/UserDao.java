a8d768946a614636092337539ca43d93cdfacbe6
AndriySV <A.S.Elips@gmail.com>
2014-09-02 10:29:43 +0300

    Create UserDao

--- src/main/java/com/softserveinc/softtour/dao/UserDao.java
+++ src/main/java/com/softserveinc/softtour/dao/UserDao.java
@@ -1,4 +1,17 @@
-package com.sovtserveinc.softtour.dao;
+package com.softserveinc.softtour.dao;
+
+import java.sql.Date;
+import java.util.List;
+
+import com.softserveinc.softtour.dto.Sex;
+import com.softserveinc.softtour.entity.User;
 
 public interface UserDao {
+	
+	public void save(String name, String e_mail, String password, Date birthday, byte age, Sex sex, String phone, long role_id);
+	public void update(long id, String name, String e_mail, String password, Date birthday, byte age, Sex sex, String phone, long role_id);
+	public void delete(long id);
+	public User findById(long id);
+	public List<User> getAll();
+	
 }
