59e19ccce87cefe685a407148eca2f9bd11787bd
KoNsyL <konsyl77@gmail.com>
2014-09-03 21:53:17 +0300

    Created interfaces Dao

---
+++ src/main/java/com/softserveinc/softtour/service/RegionService.java
@@ -0,0 +1,15 @@
+package com.softserveinc.softtour.service;
+
+import com.softserveinc.softtour.entity.Country;
+import com.softserveinc.softtour.entity.Region;
+import java.util.List;
+
+public interface RegionService {
+    public void save (String name, Country country);
+    public void update (long id, String name, Country country);
+    public void delete (long id);
+    public Region findById (long id);
+    public Region findByName (String name);
+    public List<Region> findByCountry (Country country);
+    public List<Region> getAll();
+}
