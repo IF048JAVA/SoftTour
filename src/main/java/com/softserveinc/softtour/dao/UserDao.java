682ef0da7046c8e5966b9c05d8ac6bc83761f36a
AndriySV <A.S.Elips@gmail.com>
2014-09-03 13:20:46 +0300

    change UserDao

--- src/main/java/com/softserveinc/softtour/dao/UserDao.java
+++ src/main/java/com/softserveinc/softtour/dao/UserDao.java
@@ -1,15 +1,16 @@
 package com.softserveinc.softtour.dao;
 
-import com.softserveinc.softtour.entity.User;
-import com.softserveinc.softtour.entity.template.Sex;
-
 import java.sql.Date;
 import java.util.List;
 
+import com.softserveinc.softtour.entity.Role;
+import com.softserveinc.softtour.entity.User;
+import com.softserveinc.softtour.entity.template.Sex;
+
 public interface UserDao {
 	
-	public void save(String name, String e_mail, String password, Date birthday, byte age, Sex sex, String phone, long role_id);
-	public void update(long id, String name, String e_mail, String password, Date birthday, byte age, Sex sex, String phone, long role_id);
+	public void save(String name, String email, String password, Date birthday, byte age, Sex sex, String phone, Role role);
+	public void update(long id, String name, String email, String password, Date birthday, byte age, Sex sex, String phone, Role role);
 	public void delete(long id);
 	public User findById(long id);
 	public List<User> getAll();
