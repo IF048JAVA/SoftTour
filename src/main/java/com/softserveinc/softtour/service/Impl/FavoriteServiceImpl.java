ee11ce73e5b547790339f3217ffe556eb1ca6978
"AndriySV" <"A.S.Elips@gmail.com">
2014-09-12 11:35:55 +0300

    Added the transaction to the User and Favorite classes

--- src/main/java/com/softserveinc/softtour/service/Impl/FavoriteServiceImpl.java
+++ src/main/java/com/softserveinc/softtour/service/Impl/FavoriteServiceImpl.java
@@ -3,6 +3,9 @@ package com.softserveinc.softtour.service.Impl;
 import java.sql.Date;
 import java.util.List;
 
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.softserveinc.softtour.dao.FavoriteDao;
 import com.softserveinc.softtour.entity.Favorite;
 import com.softserveinc.softtour.entity.Tour;
@@ -12,7 +15,9 @@ import com.softserveinc.softtour.service.FavoriteService;
 /**
  * @author Andriy
  * Contains the methods for work with table Favorite in the SoftTour database
+ * Supports a transaction
  */
+@Transactional(propagation=Propagation.SUPPORTS, readOnly=true)
 public class FavoriteServiceImpl implements FavoriteService{
 
 	private FavoriteDao favoriteDao;
@@ -27,8 +32,10 @@ public class FavoriteServiceImpl implements FavoriteService{
 	
 	/**
 	 * Saves the object favorite to the table Favorite
+	 * Supports a transaction
 	 */
 	@Override
+	@Transactional(propagation=Propagation.REQUIRED, readOnly=false)
 	public void save(Date date, User user, Tour tour) {
 		favoriteDao.save(date, user, tour);
 	}
@@ -36,8 +43,10 @@ public class FavoriteServiceImpl implements FavoriteService{
 	/**
 	 *  Updates the object favorite with the specified id
 	 *  id - id of the object favorite which will be updated
+	 *  Supports a transaction
 	 */
 	@Override
+	@Transactional(propagation=Propagation.REQUIRED, readOnly=false)
 	public void update(long id, Date date, User user, Tour tour) {
 		favoriteDao.update(id, date, user, tour);
 	}
@@ -45,8 +54,10 @@ public class FavoriteServiceImpl implements FavoriteService{
 	/**
 	 *  Deletes the object favorite with the specified id
 	 *  id - id of the object favorite which will be deleted
+	 *  Supports a transaction
 	 */
 	@Override
+	@Transactional(propagation=Propagation.REQUIRED, readOnly=false)
 	public void delete(long id) {
 		favoriteDao.delete(id);
 	}
@@ -54,8 +65,10 @@ public class FavoriteServiceImpl implements FavoriteService{
 	/**
 	 *  Returns the object favorite with the specified id
 	 *  id - id of the object favorite which will be returned
+	 *  Supports a transaction
 	 */
 	@Override
+	@Transactional(propagation=Propagation.REQUIRED)
 	public Favorite findById(long id) {
 		return favoriteDao.findById(id);
 	}
@@ -63,32 +76,40 @@ public class FavoriteServiceImpl implements FavoriteService{
 	/**
 	 * Returns the list of the objects favorite which contain the specified date
 	 * @param date - date of the objects which will be added to the list
+	 * Supports a transaction
 	 */
 	@Override
+	@Transactional(propagation=Propagation.REQUIRED)
 	public List<Favorite> findByDate(Date... date) {
 		return favoriteDao.findByDate(date);
 	}
 
 	/**
-	 *  Returns the list of the objects favorite which contain the specified object user 
+	 *  Returns the list of the objects favorite which contain the specified object user
+	 *  Supports a transaction 
 	 */
 	@Override
+	@Transactional(propagation=Propagation.REQUIRED)
 	public List<Favorite> findByUser(User...user) {
 		return favoriteDao.findByUser(user);
 	}
 
 	/**
-	 *  Returns the list of the objects favorite which contain the specified object tour 
+	 *  Returns the list of the objects favorite which contain the specified object tour
+	 *  Supports a transaction 
 	 */
 	@Override
+	@Transactional(propagation=Propagation.REQUIRED)
 	public List<Favorite> findByTour(Tour...tour) {
 		return favoriteDao.findByTour(tour);
 	}
 	
 	/**
 	 *  Returns the list of all objects favorite which are contained in the table Favorite
+	 *  Supports a transaction
 	 */
 	@Override
+	@Transactional(propagation=Propagation.REQUIRED)
 	public List<Favorite> getAll() {
 		return favoriteDao.getAll();
 	}
--- src/main/java/com/softserveinc/softtour/service/Impl/UserServiceImpl.java
+++ src/main/java/com/softserveinc/softtour/service/Impl/UserServiceImpl.java
@@ -3,6 +3,9 @@ package com.softserveinc.softtour.service.Impl;
 import java.sql.Date;
 import java.util.List;
 
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.softserveinc.softtour.dao.UserDao;
 import com.softserveinc.softtour.entity.Role;
 import com.softserveinc.softtour.entity.User;
@@ -12,7 +15,9 @@ import com.softserveinc.softtour.service.UserService;
 /**
  * @author Andriy
  * 	Contains the methods for work with table User in the SoftTour database
+ *  Supports a transaction
  */
+@Transactional(propagation=Propagation.SUPPORTS, readOnly = true)
 public class UserServiceImpl implements UserService {
 
 	private UserDao userDao;
@@ -27,8 +32,10 @@ public class UserServiceImpl implements UserService {
 
 	/**
 	 * Saves the object user to the table User
+	 * Supports a transaction
 	 */
 	@Override
+	@Transactional(propagation=Propagation.REQUIRED, readOnly=false)
 	public void save(String name, String email, String password, Date birthday,
 			byte age, Sex sex, String phone, Role role) {
 		userDao.save(name, email, password, birthday, age, sex, phone, role);
@@ -37,8 +44,10 @@ public class UserServiceImpl implements UserService {
 	/**
 	 *  Updates the object user with the specified id
 	 *  id - id of the object user which will updated
+	 *  Supports a transaction
 	 */
 	@Override
+	@Transactional(propagation=Propagation.REQUIRED, readOnly=false)
 	public void update(long id, String name, String email, String password,
 			Date birthday, byte age, Sex sex, String phone, Role role) {
 
@@ -49,8 +58,10 @@ public class UserServiceImpl implements UserService {
 	/**
 	 *  Deletes the object user with the specified id
 	 *  id - id of the object user which will deleted
+	 *  Supports a transaction
 	 */
 	@Override
+	@Transactional(propagation=Propagation.REQUIRED, readOnly=false)
 	public void delete(long id) {
 		userDao.delete(id);
 	}
@@ -58,79 +69,100 @@ public class UserServiceImpl implements UserService {
 	/**
 	 *  Returns the object user with the specified id
 	 *  id - id of the object user which will returned
+	 *  Supports a transaction
 	 */
 	@Override
+	@Transactional(propagation=Propagation.REQUIRED)
 	public User findById(long id) {
 		return userDao.findById(id);
 	}
 
 	/**
 	 *  Returns list of the objects user with the specified name or names
+	 *   Supports a transaction
 	 */
 	@Override
+	@Transactional(propagation=Propagation.REQUIRED)
 	public List<User> findByName(String...name) {
 		return userDao.findByName(name);
 	}
 	
 	/**
 	 *  Returns list of the objects user with the specified email or emails
+	 *   Supports a transaction
 	 */
 	@Override
+	@Transactional(propagation=Propagation.REQUIRED)
 	public List<User> findByEmail(String... email) {
 		return userDao.findByEmail(email);
 	}
 	
 	/**
 	 *  Returns list of the objects user with the specified password or passwords
+	 *   Supports a transaction
 	 */
+	@Override
+	@Transactional(propagation=Propagation.REQUIRED)
 	public List<User> findByPassword(String... password) {
 		return userDao.findByPassword(password);
 	}
 	
 	/**
 	 *  Returns list of the objects user with the specified birthday or birthdays
+	 *   Supports a transaction
 	 */
 	@Override
+	@Transactional(propagation=Propagation.REQUIRED)
 	public List<User> findByBirthday(Date... birthday) {
 		return userDao.findByBirthday(birthday);
 	}
 	
 	/**
 	 *  Returns list of the objects user with the specified age
+	 *   Supports a transaction
 	 */
 	@Override
+	@Transactional(propagation=Propagation.REQUIRED)
 	public List<User> findByAge(Byte... age) {
 		return userDao.findByAge(age);
 	}
 	
 	/**
 	 *  Returns list of the objects user with the specified sex
+	 *   Supports a transaction
 	 */
 		@Override
+		@Transactional(propagation=Propagation.REQUIRED)
 	public List<User> findBySex(Sex sex) {
 		return userDao.findBySex(sex);
 	}
 		
 	/**
 	 *  Returns list of the objects user with the specified phone or phones
+	 *   Supports a transaction
 	 */
 	@Override
+	@Transactional(propagation=Propagation.REQUIRED)
 	public List<User> findByPhone(String... phone) {
 		return userDao.findByPhone(phone);
 	}
 		
 	/**
 	 *  Returns the list of the objects user which contain the specified object or objects role
+	 *   Supports a transaction
 	 */
 	@Override
+	@Transactional(propagation=Propagation.REQUIRED)
 	public List<User> findByRole(Role...role) {
 		return userDao.findByRole(role);
 	}
 	
 	/**
 	 *  Returns the list of all objects user which are contained in the table User
+	 *   Supports a transaction
 	 */
 	@Override
+	@Transactional(propagation=Propagation.REQUIRED)
 	public List<User> getAll() {
 		return userDao.getAll();
 	}
