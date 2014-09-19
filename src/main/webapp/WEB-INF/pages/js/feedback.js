9cfaed979251c2d40b9563dcfa650ee7acce8d30
YuriyValkiv <yurivalkiv@gmail.com>
2014-09-19 21:25:02 +0300

    add feedback.js

---
+++ src/main/webapp/WEB-INF/pages/js/feedback.js
@@ -0,0 +1,17 @@
+﻿function checkForm(user_name, user_email, user_area){
+	name = document.getElementById(user_name).value;
+	email = document.getElementById(user_email).value;
+	area = document.getElementById(user_area).value;
+    reg = "/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/";
+	
+	if(name.length<1){
+		alert("Введіть коректне ім'я");
+		return false;
+	}else if(reg.match(email)){
+		alert("Невірний e-mail");
+		return false;
+	}else if(area.length<1){
+		alert("Введіть коментар");
+		return false;
+	}
+}
