3af375ad9a308ce6daaea9d52e16a448830a7b90
Boichuk20 <konsyl77@gmail.com>
2014-09-19 17:31:17 +0300

    added calendar.js

---
+++ src/main/webapp/WEB-INF/pages/js/calendar.js
@@ -0,0 +1,12 @@
+
+                                        // When the document is ready
+                                        $(document).ready(function () {
+                                            
+                                            $('#birthdayInput').datepicker({
+                                            
+                                                format: 'dd/mm/yyyy'
+                                                
+
+                                            }); 
+                                            
+                                        });  
