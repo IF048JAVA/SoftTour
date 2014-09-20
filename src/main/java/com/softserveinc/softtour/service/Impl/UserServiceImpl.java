f663f7f46a8e119800a493f0252954ba488cc043
"AndriySV" <"A.S.Elips@gmail.com">
2014-09-20 18:31:39 +0300

    Correct user and favorite

--- src/main/java/com/softserveinc/softtour/service/Impl/UserServiceImpl.java
+++ src/main/java/com/softserveinc/softtour/service/Impl/UserServiceImpl.java
@@ -77,7 +77,7 @@ public class UserServiceImpl implements UserService {
 	@Transactional(propagation=Propagation.REQUIRED)
 	public List<User> findByAnyParameters(long id, String name, String email, String password, 
 								Date birthday, byte age, Sex sex, String phone, Role role) {
-		return userRepository.findByIdOrNameOrEmailOrPasswordOrBirthdayOrAgeOrSexOrPhoneOrRole(
+		return userRepository.findByIdAndNameAndEmailAndPasswordAndBirthdayAndAgeAndSexAndPhoneAndRole(
 				id, name, email, password, birthday, age, sex, phone, role);
 	}
 	
