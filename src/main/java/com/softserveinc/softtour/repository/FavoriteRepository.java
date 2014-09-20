95415988652a394ec66d08eaf0b05e63bfa6f447
"AndriySV" <"A.S.Elips@gmail.com">
2014-09-20 15:27:05 +0300

    Updated FavoriteRepository

--- src/main/java/com/softserveinc/softtour/repository/FavoriteRepository.java
+++ src/main/java/com/softserveinc/softtour/repository/FavoriteRepository.java
@@ -8,60 +8,30 @@ import java.sql.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * @author Andriy
  * Contains the methods for work with table Favorite in the SoftTour database
  */
-@Repository
 public interface FavoriteRepository extends JpaRepository<Favorite, Long>{
 	
 	/**
-	 * Saves the object favorite to the table Favorite
-	 */
-	//@Query
-	public void save(Date date, User user, Tour tour);
-	
-	/**
 	 *  Updates the object favorite with the specified id
-	 *  id - id of the object favorite which will be updated
+	 * @param id - id of the object favorite which will be updated
+	 * @param date - updated date
+	 * @param user - updated user
+	 * @param tour - updated tour
 	 */
-	//@Query
+	@Modifying
+	@Query("UPDATE Favorite SET date = ?2, user_id = ?3, tour_id = ?4 WHERE id = ?1")
 	public void update(long id, Date date, User user, Tour tour);
 	
 	/**
-	 *  Deletes the object favorite with the specified id
-	 *  id - id of the object favorite which will be deleted
+	 * Returns the list of the favorite's objects with the specified parameters
 	 */
-	//@Query
-	public void delete(long id);
-	
-	/**
-	 *  Returns the object favorite with the specified id
-	 *  id - id of the object favorite which will be returned
-	 */
-	public Favorite findById(long id);
-	
-	/**
-	 * Returns the list of the objects favorite which contain the specified date
-	 * @param date - date of the objects which will be added to the list
-	 */
-	public List<Favorite> findByDate(Date...date);
-	
-	/**
-	 *  Returns the list of the objects favorite which contain the specified object user 
-	 */
-	public List<Favorite> findByUser (User...user);
-	
-	/**
-	 *  Returns the list of the objects favorite which contain the specified object tour 
-	 */
-	public List<Favorite> findByTour (Tour...tour);
-	
-	/**
-	 *  Returns the list of all objects favorite which are contained in the table Favorite
-	 */
-	public List<Favorite> getAll();	
+	public List<Favorite> findByIdOrDateOrUserOrTour(
+			long id, Date date, User user, Tour tour);
 	
 }
