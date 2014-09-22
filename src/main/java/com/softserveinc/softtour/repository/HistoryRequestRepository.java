0adc10a3f66d89634860ea5c499857f1058db2ea
Taras <Off.bk@bk.ru>
2014-09-22 22:17:32 +0300

    add HistoryRequest JPA classes

--- src/main/java/com/softserveinc/softtour/repository/HistoryRequestRepository.java
+++ src/main/java/com/softserveinc/softtour/repository/HistoryRequestRepository.java
@@ -1,21 +1,19 @@
-package com.softserveinc.softtour.service;
+package com.softserveinc.softtour.repository;
+
 
 import com.softserveinc.softtour.entity.Country;
 import com.softserveinc.softtour.entity.HistoryRequest;
 import com.softserveinc.softtour.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Date;
 import java.util.List;
 
-public interface HistoryRequestService {
+public interface HistoryRequestRepository extends JpaRepository<HistoryRequest, Long> {
 
-    public void save(HistoryRequest historyRequest);
-    public void delete(long id);
-    public HistoryRequest findById(long id);
     public List<HistoryRequest> findByUser(User user);
     public List<HistoryRequest> findByStars(int... stars);
     public List<HistoryRequest> findByCountry(Country country);
     public List<HistoryRequest> findByRequestDate(Date requestDate);
-    public List<HistoryRequest> getAll();
 
 }
