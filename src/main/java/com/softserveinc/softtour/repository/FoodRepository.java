ff7e6aa01817fc81334befbe2e14c3f6de85567d
YuriyValkiv <yurivalkiv@gmail.com>
2014-09-22 23:09:00 +0300

    change

--- src/main/java/com/softserveinc/softtour/repository/FoodRepository.java
+++ src/main/java/com/softserveinc/softtour/repository/FoodRepository.java
@@ -10,9 +10,4 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface FoodRepository extends JpaRepository<Food, Long> {
 
-    @Modifying
-    @Query("UPDATE Food SET name=?2 WHERE id=?1")
-    public void update(long id, String name);
-
-    public List<Food> findByIdOrName(long id, String name);
 }
