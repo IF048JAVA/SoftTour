7f4b1e737f27a0c4eca7dbfd04ded78fd36a68ea
"AndriySV" <"A.S.Elips@gmail.com">
2014-09-22 14:35:47 +0300

    Edited UserController

--- src/main/java/com/softserveinc/softtour/controller/UserController.java
+++ src/main/java/com/softserveinc/softtour/controller/UserController.java
@@ -2,9 +2,11 @@ package com.softserveinc.softtour.controller;
 
 import java.sql.Date;
 import java.util.Calendar;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,7 +18,7 @@ import com.softserveinc.softtour.service.UserService;
 
 /**
  * @author Andriy
- * Gets the data about user and processes their
+ * Processes user's data
  */
 @Controller
 @RequestMapping(value="/user")
@@ -35,14 +37,14 @@ public class UserController {
 	 * and which we need to cast to the type Sex 
 	 * @return the name of the main page 
 	 */
-	@RequestMapping(value="/save", method=RequestMethod.GET)
-	public String saveUser(User user, @RequestParam String sexString) {
+	@RequestMapping(value="/save", method=RequestMethod.POST)
+	public String save(User user, @RequestParam String sexString) {
 		
 		user.setAge(calculateAge(user.getBirthday()));
 		user.setSex(defineSex(sexString));
 		
-		// Need to use --  Role role = roleServise.findById(3) !!!
-		// Need to think  how to define role !!!
+		// FIXME Need to use --  Role role = roleServise.findByName("registeredUser") !!!
+		// Temporary code ...   
 		Role role = new Role();
 		role.setId(3);
 		role.setName("registeredUser");
@@ -50,7 +52,88 @@ public class UserController {
 		
 		userService.save(user);
 		
-		return "index";
+		return "redirect:/";
+	}
+	
+	@RequestMapping(value="/update", method=RequestMethod.POST)
+	public String update(User user, @RequestParam String sexString, Model model) {
+		
+		// FIXME Need to set id !?
+		long id= 0;
+		
+		user.setAge(calculateAge(user.getBirthday()));
+		user.setSex(defineSex(sexString));
+		
+		// FIXME Need to use --  Role role = roleServise.findByName("registeredUser") !!!
+		// Temporary code ... !
+		Role role = new Role();
+		role.setId(3);
+		role.setName("registeredUser");
+		user.setRole(role);		
+		
+		userService.update(id, user);
+		
+		return "redirect:/userProfile";
+	}
+	
+	/**
+	 * Deletes the object user with the specified id
+	 * @param idString - id of the user's object which will be deleted
+	 * @return the name of the main page 
+	 */
+	@RequestMapping(value="/delete", method=RequestMethod.GET)
+	public String delete(@RequestParam String idString) {
+		
+		long id = Long.parseLong(idString);
+		userService.delete(id);
+		
+		return "redirect:/index";
+	}
+	
+	/**
+	 * Returns the object user with the specified id
+	 * @param idString - id of the user's object which will be returned
+	 * @param model - it's response in which we write the user's object with the specified id
+	 * @return the name of the main page 
+	 */
+	@RequestMapping(value="/findById", method=RequestMethod.GET)
+	public String findById(@RequestParam String idString, Model model) {
+		
+		long id = Long.parseLong(idString);
+		User user = userService.findById(id);
+		model.addAttribute("user", user);
+		
+		return "redirect:/index";
+	}
+	
+	/**
+	 * Returns the list of the user's objects with the specified parameters
+	 * @param user - contains the parameters to search
+	 * @param model - it's response in which we write the user's object with the specified id
+	 * @return the name of the main page 
+	 */
+	@RequestMapping(value="/findByAnyParameters", method=RequestMethod.GET)
+	public String findByAnyParameters(User user, Model model) {
+		
+		List<User> list = userService.findByAnyParameters(user.getId(), user.getName(),
+				user.getEmail(), user.getPassword(), user.getBirthday(), user.getAge(),
+				user.getSex(), user.getPhone(), user.getRole());
+		
+		model.addAttribute("users", list);
+		return "redirect:/index";
+	}
+
+	/**
+	 * Returns the list of all user's objects which are contained in the table User
+	 * @param model - it's response in which we write all user's objects which are contained in the table User
+	 * @return the name of the main page 
+	 */
+	@RequestMapping(value="/findAll")
+	public String findAll(Model model) {
+		List<User> list = userService.findAll();
+		model.addAttribute("allUsers", list);
+		
+		return "redirect:/index";
 	}
 	
 	/**
