2cad39dbd73b853540ca5aa4f68b732871480e45
KoNsyL <konsyl77@gmail.com>
2014-09-23 14:22:58 +0300

    Dao -> Jpa

--- src/main/java/com/softserveinc/softtour/service/RegionService.java
+++ src/main/java/com/softserveinc/softtour/service/RegionService.java
@@ -1,15 +1,21 @@
 package com.softserveinc.softtour.service;
 
-import com.softserveinc.softtour.entity.Country;
+
 import com.softserveinc.softtour.entity.Region;
+
 import java.util.List;
 
 public interface RegionService {
-    public void save (String name, Country country);
-    public void update (long id, String name, Country country);
-    public void delete (long id);
-    public Region findById (long id);
-    public List<Region> findByName (String name);
-    public List<Region> findByCountry (Country country);
-    public List<Region> getAll();
+
+    Region save(Region region);
+
+    Region findOne(long id);
+
+    List<Region> findAll();
+
+    void delete(Region region);
+
+    void deleteById(long id);
+
+    List<Region> findByCustomParameters();
 }
