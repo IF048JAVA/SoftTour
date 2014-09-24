c66e43e58aa1a5b20622ce684413302972da70ae
oleksandrgasenuk <a.gashenyuk@gmail.com>
2014-09-24 15:58:40 +0300

    findAll

--- src/main/java/com/softserveinc/softtour/service/Impl/TourServiceImpl.java
+++ src/main/java/com/softserveinc/softtour/service/Impl/TourServiceImpl.java
@@ -1,8 +1,11 @@
 package com.softserveinc.softtour.service.Impl;
 
+import com.softserveinc.softtour.entity.Tour;
 import com.softserveinc.softtour.repository.TourRepository;
 import com.softserveinc.softtour.service.TourService;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 /**
  * Created by oleksandrgasenuk on 04.09.14.
@@ -17,7 +20,7 @@ public class TourServiceImpl implements TourService {
     }
 
     @Override
-    public void findAll() {
+    public List<Tour> findAll() {
         tourRepository.findAll();
     }
 }
--- src/main/java/com/softserveinc/softtour/service/TourService.java
+++ src/main/java/com/softserveinc/softtour/service/TourService.java
@@ -1,8 +1,12 @@
 package com.softserveinc.softtour.service;
 
+import com.softserveinc.softtour.entity.Tour;
+
+import java.util.List;
+
 /**
  * Created by oleksandrgasenuk on 04.09.14.
  */
 public interface TourService {
-    void findAll();
+    List<Tour> findAll();
 }
