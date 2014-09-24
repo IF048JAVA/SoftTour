d734e00cef8e1f13d3f70c23c265d02050e0ed7b
oleksandrgasenuk <a.gashenyuk@gmail.com>
2014-09-24 15:39:37 +0300

    parsers improves

---
+++ src/main/java/com/softserveinc/softtour/repository/TourRepository.java
@@ -0,0 +1,12 @@
+package com.softserveinc.softtour.repository;
+
+import com.softserveinc.softtour.entity.Tour;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+/**
+ * Created by oleksandrgasenuk on 04.09.14.
+ */
+public interface TourRepository extends JpaRepository<Tour, Long> {
+    List<Tour> findAll();
+}
