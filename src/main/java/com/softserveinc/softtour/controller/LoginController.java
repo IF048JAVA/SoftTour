c38316694da57c8ece78d491b8ccacf28ef38938
AndriySV <A.S.Elips@gmail.com>
2014-10-14 10:11:02 +0300

    Refactored Login controller and remoted Login Validator

--- src/main/java/com/softserveinc/softtour/controller/LoginController.java
+++ src/main/java/com/softserveinc/softtour/controller/LoginController.java
@@ -3,21 +3,16 @@ package com.softserveinc.softtour.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.servlet.ModelAndView;
 
-import com.softserveinc.softtour.entity.User;
 import com.softserveinc.softtour.service.UserService;
-import com.softserveinc.softtour.util.LoginValidator;
 
+/**
+ * @author Andrii
+ * Logins a user
+ */
 @Controller
-@RequestMapping(value="/login")
-@SessionAttributes ({"user"})
 public class LoginController {
 	
 	/**
@@ -26,34 +21,17 @@ public class LoginController {
 	@Autowired
 	private UserService userService;
 	
-	@Autowired
-	private LoginValidator loginValidator;
-	
-	@Autowired
-	private UserProfileConroller userProfileConroller;
-
-	@RequestMapping(value="/form")	
+	/**
+	 * Logins a user
+	 * @return the name which redirect to the page login.jsp
+	 */
+	@RequestMapping(value="/login")	
 	public String loginUser (@RequestParam(value = "error", required = false) String error,
 			@RequestParam(value = "logout", required = false) String logout, Model model){
 		
 		if (error != null) {
 			model.addAttribute("error", "Invalid email or password!");
 		}
-
 		return "login";
-	}
-	
-	//FIXME  Is this method  need ?!
-	@RequestMapping(value="/enter", method=RequestMethod.POST)	
-	public String enter (User user, BindingResult bindingResult, ModelMap model){
-		loginValidator.validate(user, bindingResult);
-		
-		if (!bindingResult.hasErrors()) {
-			model.addAttribute(user);
-			
-			return "redirect:/userProfile";
-		}else {
-			return "login";
-		}
 	}
 }
