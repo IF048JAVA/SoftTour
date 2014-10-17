9c8f7ae055b412c5c23c53ae495fa99238d7db53
AndriySV <A.S.Elips@gmail.com>
2014-10-17 21:13:58 +0300

    Edited train parser

--- src/main/java/com/softserveinc/softtour/parsers/impl/TrainParser.java
+++ src/main/java/com/softserveinc/softtour/parsers/impl/TrainParser.java
@@ -9,116 +9,121 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import com.softserveinc.softtour.dto.TrainTransit;
+import com.softserveinc.softtour.util.CreatorTrainUrl;
 
 public class TrainParser {
-	private String url = "http://ticket.turistua.com/ua/train/reservation/?dt=2014-10-18&src=22200001&dst=22218000&transport=train";
+
+	String id;
+	String departureCity;
+	String arrivalCity;
+	String departureTime;
+	String arrivalTime;
+	String priceMax;
+	String priceMin;
 	
-	//FIXME correct
-	private String departureDate = "2014-10-18";
+	private String departureDate;
 	
 	private TrainTransit trainTransit;
-	private ArrayList<TrainTransit> schedule = new ArrayList<TrainTransit>();
+	private ArrayList<TrainTransit> routsList = new ArrayList<TrainTransit>();
 	
+	private String url;
 	
-	public static void main(String[] args) {
-		TrainParser obj = new TrainParser();
-		obj.parse();
+	public TrainParser(String departureCity, String arrivalCity, String departureDate) {
+		this.departureDate = departureDate;
+		this.url = CreatorTrainUrl.createUrl(departureCity, arrivalCity, departureDate); 
 	}
-
-	private void parse() {
+	
+	private Document document;
+	
+	public ArrayList<TrainTransit> getRouts() {
+		parse();
+		setRoutes();
+		
+		return routsList; 
+	}
+	
+	public void parse() {
 		try {
-			String doc = Jsoup.connect(url).timeout(5000)
-					.ignoreContentType(true).execute().body();
-			Document document = Jsoup.parse(doc);
-
-			Element trainsTable = document.getElementsByTag("body").get(0)
-					.getElementById("turistua_site")
-					.getElementById("content")
-					.getElementsByClass("reservationCol").get(0)
-					.getElementsByClass("ticketsBox").get(0)
-					.getElementById("t2t_tableBox")
-					.getElementsByTag("table").get(0);
+			String htmlDocument = Jsoup.connect(url)
+										.timeout(5000)
+										.ignoreContentType(true)
+										.execute()
+										.body();
 			
-			Elements trains = trainsTable.select("tr[class]");
-
-			for (Element train : trains) {
-
-				Elements td = train.getElementsByTag("td");
-
-				String id = null;
-				String departureCity = null;
-				String arrivalCity = null;
-				String departureTime = null;
-				String arrivalTime = null;
-				
-				String priceLux = null;
-				String priceCoupeFirm = null;
-				String priceCoupe = null;
-				String priceBerthFirm = null;
-				String priceBerth = null;
-				String priceSitting = null;
-				
-				for (int i = 0; i < td.size(); i++) {
-					int n = 0;
-					
-					id = td.get(n).text();
-					departureCity = td.get(n).text();
-					arrivalCity = td.get(++n).text();
-					departureTime = td.get(++n).text();
-					arrivalTime = td.get(n = n + 2).text();
-					
-					priceLux = setPrice(td, ++n);
-					priceCoupeFirm = setPrice(td, ++n);
-					priceCoupe = setPrice(td, ++n);
-					priceBerthFirm = setPrice(td, ++n);
-					priceBerth = setPrice(td, ++n);
-					priceSitting = setPrice(td, ++n);
-				
-				}
-				
-				//FIXME min price
-					String priceFrom = null;
-					if (priceSitting != null) {
-						priceFrom = priceSitting;
-					} else if (priceBerth != null) {
-						priceFrom = priceBerth;
-					} else if (priceBerthFirm != null) {
-						priceFrom = priceBerthFirm;
-					} else if (priceCoupe != null) {
-						priceFrom = priceCoupe;
-					} else if (priceCoupeFirm != null) {
-						priceFrom = priceCoupeFirm;
-					} else if (priceLux != null) {
-						priceFrom = priceLux;
-					}
-					
-				// FIXME max price
-					String priceTo = null;
-					if (priceLux != null) {
-						priceTo = priceLux;
-					} else if (priceCoupeFirm != null) {
-						priceTo = priceCoupeFirm;
-					} else if (priceCoupe != null) {
-						priceTo = priceCoupe;
-					} else if (priceBerthFirm != null) {
-						priceTo = priceBerthFirm;
-					} else if (priceBerth != null) {
-						priceTo = priceBerth;
-					} else if (priceSitting != null) {
-						priceTo = priceSitting;
-					} 
-					
-					trainTransit = new TrainTransit(id, departureCity, arrivalCity, departureDate, departureTime, arrivalTime, priceFrom, priceTo);							
-							
-				    System.out.println(trainTransit);
-				    schedule.add(trainTransit);
-			}
-
+			document = Jsoup.parse(htmlDocument);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
+		
+	private void setRoutes(){
+		Element routesTable = document.getElementsByTag("body").get(0)
+				.getElementById("turistua_site")
+				.getElementById("content")
+				.getElementsByClass("reservationCol").get(0)
+				.getElementsByClass("ticketsBox").get(0)
+				.getElementById("t2t_tableBox")
+				.getElementsByTag("table").get(0);
+			
+		Elements routes = routesTable.select("tr[class]");
+		setRoute(routes);
+	}
 	
+	private void setRoute(Elements routes) {
+		for (Element route : routes) {
+			Elements info = route.getElementsByTag("td");
+			setInfo(info);
+			
+			addRoute();
+				    System.out.println(trainTransit);
+				    routsList.add(trainTransit);
+		}
+		
+	}
+
+	private void addRoute() {
+		routsList.add(new TrainTransit(id, departureCity, arrivalCity, departureDate, departureTime, arrivalTime, priceMin, priceMax));
+		
+		
+	}
+
+	private void setInfo(Elements info) {
+		
+		
+		//FIXME maybe need to change digit 6 to same variables !
+		// from priceLux to priceSitting  
+		String[] prices = new String[6];
+		
+		for (int i = 0; i < info.size(); i++) {
+			int n = 0;
+			
+			id = info.get(n).text();
+			departureCity = info.get(++n).text();
+			arrivalCity = info.get(++n).text();
+			departureTime = info.get(++n).text();
+			arrivalTime = info.get(n = n + 2).text();
+			
+			// Sets prices massif from max to min proce
+			for (int j = 0; j < prices.length; j++) {
+				prices[j] = setPrice(info, ++n);
+			}
+		}
+		
+		
+		for (int i = 0; i < prices.length; i++) {
+			if (prices[i] != null) {
+				priceMax = prices[i];
+			}
+		}
+		
+		
+		for (int i = prices.length; i < 0 ; i--) {
+			if (prices[i] != null) {
+				priceMin = prices[i];
+			}
+		}
+	}
+
 	private String setPrice(Elements td, int n) {
 		String price = null;
 		Elements priceElement = td.get(n).getElementsByTag("span");
@@ -127,5 +132,11 @@ public class TrainParser {
 		}
 		return price;
 	}
+	
+	public static void main(String[] args) {
+	//	TrainParser obj = new TrainParser(departureCity, arrivalCity, departureTime)
+//		obj.parse();
+	}
+
 
 }
