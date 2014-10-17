d73439062cdf5a7acbdc1414792ba5de90033d2b
AndriySV <A.S.Elips@gmail.com>
2014-10-17 14:22:19 +0300

    Added new TrainTransit class

--- src/main/java/com/softserveinc/softtour/dto/TrainTransit.java
+++ src/main/java/com/softserveinc/softtour/dto/TrainTransit.java
@@ -1,63 +1,69 @@
 package com.softserveinc.softtour.dto;
 
-import java.util.Date;
-
 public class TrainTransit {
 
-    private String cityFrom;
-    private String cityTo;
-    private Date departureTime;
-    private Date arrivalTime;
-    private Integer price;
-
-    public String getCityFrom() {
-        return cityFrom;
+    private String departureCity;
+    private String arrivalCity;
+    private String departureTime;
+    private String arrivalTime;
+    private String priceFrom;
+    private String priceTo;
+    
+    public TrainTransit() {
     }
+    
+	public TrainTransit(String departureCity, String arrivalCity,
+			String departureTime, String arrivalTime, String priceFrom,
+			String priceTo) {
+		this.departureCity = departureCity;
+		this.arrivalCity = arrivalCity;
+		this.departureTime = departureTime;
+		this.arrivalTime = arrivalTime;
+		this.priceFrom = priceFrom;
+		this.priceTo = priceTo;
+	}
 
-    public void setCityFrom(String cityFrom) {
-        this.cityFrom = cityFrom;
-    }
+	public String getDepartureCity() {
+		return departureCity;
+	}
+	public void setDepartureCity(String departureCity) {
+		this.departureCity = departureCity;
+	}
+	public String getArrivalCity() {
+		return arrivalCity;
+	}
+	public void setArrivalCity(String arrivalCity) {
+		this.arrivalCity = arrivalCity;
+	}
+	public String getDepartureTime() {
+		return departureTime;
+	}
+	public void setDepartureTime(String departureTime) {
+		this.departureTime = departureTime;
+	}
+	public String getArrivalTime() {
+		return arrivalTime;
+	}
+	public void setArrivalTime(String arrivalTime) {
+		this.arrivalTime = arrivalTime;
+	}
+	public String getPriceFrom() {
+		return priceFrom;
+	}
+	public void setPriceFrom(String priceFrom) {
+		this.priceFrom = priceFrom;
+	}
+	public String getPriceTo() {
+		return priceTo;
+	}
+	public void setPriceTo(String priceTo) {
+		this.priceTo = priceTo;
+	}
 
-    public String getCityTo() {
-        return cityTo;
-    }
-
-    public void setCityTo(String cityTo) {
-        this.cityTo = cityTo;
-    }
-
-    public Date getDepartureTime() {
-        return departureTime;
-    }
-
-    public void setDepartureTime(Date departureTime) {
-        this.departureTime = departureTime;
-    }
-
-    public Date getArrivalTime() {
-        return arrivalTime;
-    }
-
-    public void setArrivalTime(Date arrivalTime) {
-        this.arrivalTime = arrivalTime;
-    }
-
-    public Integer getPrice() {
-        return price;
-    }
-
-    public void setPrice(Integer price) {
-        this.price = price;
-    }
-
-    @Override
-    public String toString() {
-        return "TrainTransit{" +
-                "cityFrom='" + cityFrom + '\'' +
-                ", cityTo='" + cityTo + '\'' +
-                ", departureTime=" + departureTime +
-                ", arrivalTime=" + arrivalTime +
-                ", price=" + price +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return departureCity + " - " + arrivalCity + " - " + departureTime + " - " + arrivalTime 
+				+ " - " + priceFrom + " - " + priceTo;
+	}
+	
 }
