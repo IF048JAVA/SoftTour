f19d7ced7bc45b52e1728187a652c76a26ff296b
AndriySV <A.S.Elips@gmail.com>
2014-10-19 19:04:05 +0300

    Created train_parser.properties and refactored CreatorTrainUrl

--- src/main/java/com/softserveinc/softtour/util/CreatorTrainUrl.java
+++ src/main/java/com/softserveinc/softtour/util/CreatorTrainUrl.java
@@ -1,12 +1,62 @@
 package com.softserveinc.softtour.util;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 public class CreatorTrainUrl {
-	//private String url = "http://ticket.turistua.com/ua/train/reservation/?dt=2014-10-18&src=22200001&dst=22218000&transport=train";
-	
-	public static String createUrl(String departureCity, String arrivalCity, String departureDate) {
-		String url = null;
-		
-		
+
+	//FIXME  need static ???
+	/**
+	 * Creates the url with specified parameters
+	 * 
+	 * @param departureCity
+	 * @param arrivalCity
+	 * @param departureDate
+	 * @return
+	 */
+	public static String createUrl(String departureCity, String arrivalCity,
+			String departureDate) {
+		StringBuilder baseUrl = new StringBuilder("http://ticket.turistua.com/ua/train/reservation/?transport=train");
+
+		String url = baseUrl.append("&dt=").append(departureDate)
+							.append("&src=").append(getCityCode(departureCity))
+							.append("&dst=").append(getCityCode(arrivalCity))
+							.toString();
 		return url;
 	}
+
+	private static String getCityCode(String city) {
+		Properties properties = new Properties();
+		FileReader fileReader = null;
+		BufferedReader bufferedReader = null;
+
+		try {
+			// FIXME change url
+			fileReader = new FileReader("src/main/resources/parser_properties/train_parser.properties");
+			bufferedReader = new BufferedReader(fileReader);
+			properties.load(bufferedReader);
+
+			return properties.getProperty(city);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (fileReader != null) {
+					fileReader.close();
+				}
+				if (bufferedReader != null) {
+					bufferedReader.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+
 }
